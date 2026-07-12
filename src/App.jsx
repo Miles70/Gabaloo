c374aa1e270838d6739c2f20c383c82be347370a
Miles70 <Selimyalcinner@gmail.com>
2026-07-12 18:40:54 +0300

    Load live search styles

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import AdminSettings from "./pages/admin/AdminSettings";
 import AdminProtectedRoute from "./components/admin/AdminProtectedRoute";
 import CartToast from "./components/CartToast/CartToast";
 import "./styles/store-products.css";
+import "./styles/search-live.css";
 import "./styles/admin.css";
 import "./styles/admin-enhancements.css";
 import "./styles/admin-pagination.css";
