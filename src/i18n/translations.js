2cbb1077c4f36fbbed1d8581020ac9e6840b39b3
Miles70 <Selimyalcinner@gmail.com>
2026-07-13 18:31:22 +0300

    Merge payment translations into locales

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -3,13 +3,30 @@ import tr from "./locales/tr";
 import ru from "./locales/ru";
 import ar from "./locales/ar";
 import zh from "./locales/zh";
+import paymentTranslations from "./paymentTranslations";
+
+function withPaymentTranslations(baseTranslations, language) {
+  const payment = paymentTranslations[language] || paymentTranslations.en;
+
+  return {
+    ...baseTranslations,
+    checkoutPage: {
+      ...baseTranslations.checkoutPage,
+      ...payment.checkoutPage,
+    },
+    orderSuccessPage: {
+      ...baseTranslations.orderSuccessPage,
+      ...payment.orderSuccessPage,
+    },
+  };
+}
 
 const translations = {
-  en,
-  tr,
-  ru,
-  ar,
-  zh,
+  en: withPaymentTranslations(en, "en"),
+  tr: withPaymentTranslations(tr, "tr"),
+  ru: withPaymentTranslations(ru, "ru"),
+  ar: withPaymentTranslations(ar, "ar"),
+  zh: withPaymentTranslations(zh, "zh"),
 };
 
 export default translations;
